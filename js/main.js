2484c8c30f4e211f33594b2500fa5872b32ae1b2
unknown <marwan.nasser565@gamil.com>
2023-02-18 12:46:11 +0200

    last update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// $(function () {
+//   $( "#slider-range" ).slider({
+//     range: true,
+//     min: 0,
+//     max: 5000,
+//     values: [ 0, 2500 ],
+//     slide: function( event, ui ) {
+//       $('.min-val').text("$" + ui.values[ 0 ])
+//       $('.max-val').text("$" + ui.values[ 1 ])
+//     }
+//   });
+ 
+    
+// });
